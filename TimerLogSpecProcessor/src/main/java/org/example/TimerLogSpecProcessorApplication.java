ab3189f4c90f8745262b73a1ab9849000b75c94c
Aliia Forostiak <aforostyak@gmail.com>
2026-06-22 13:59:45 +0500

    Refactor TimerLog spec route

--- TimerLogSpecProcessor/src/main/java/org/example/TimerLogSpecProcessorApplication.java
+++ TimerLogSpecProcessor/src/main/java/org/example/TimerLogSpecProcessorApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TimerLogApplication {
+public class TimerLogSpecProcessorApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TimerLogApplication.class, args);
+        SpringApplication.run(TimerLogSpecProcessorApplication.class, args);
     }
 }
